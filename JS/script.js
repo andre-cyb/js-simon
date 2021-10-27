ce1912cad6826aa1a097a515a8ca3fc1cea28e74
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-10-27 14:00:50 +0200

    creato array di numeri casuali e alert

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,29 @@
-"use strict"
+//fare partire un countdown di 30000 ms dove viene mostrato un alert con dei numeri casuali
+//allo scadere del timer chiedere i numeri che si sono visti precedentemente
+
+"use strict";
+
+
+
+
+let arrayNumeri = [];
+
+for (let i = 0; i < 5; i++) {
+    let numero = randomNumber();
+    arrayNumeri.push(numero);
+}
+console.log(arrayNumeri);
+
+alert("ricorda questi numeri, hai 30 secondi di tempo a disposizione: " + arrayNumeri);
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////7
+//FUNZIONI
+function randomNumber() {
+    return Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+}
+/* console.log(randomNumber()); */
